84d514ff1b119f7013ea9dc4146982d9449ace8e
kingraham228 <kate.ingraham@knights.ucf.edu>
2021-05-27 22:39:56 -0400

    fixed input.nextLine()

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -6,14 +6,15 @@ public class App
     public static void main( String[] args )
     {
 
-        Scanner input = new Scanner(System.in).useDelimiter("\\n");
+        Scanner input = new Scanner(System.in);
         System.out.println("What is the quote? ");
-        String userQuote = input.next();
+        String userQuote = input.nextLine();
 
         System.out.println("Who said it? ");
-        String userAuthor = input.next();
+        String userAuthor = input.nextLine();
 
-        System.out.printf("What is the quote? %s%nWho said it? %s%n%s says, \"%s\"", userQuote, userAuthor, userAuthor, userQuote);
+        System.out.printf("What is the quote? %s%nWho said it? %s%n%s says, \"%s\"%n",
+                userQuote, userAuthor, userAuthor, userQuote);
 
 
     }
